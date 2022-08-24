2fa273f8b81ab3f74b7137f469a71a5bba9fa346
jss118 <joesmith_guitar@hotmail.co.uk>
2022-08-24 12:25:17 +0100

    add inc_votes functionality

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { fetchSingleArticle } from "../utils/api";
 import { useParams } from "react-router-dom";
+import { updateVote } from "../utils/api";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [votes, setVotes] = useState(0);
+  const [err, setErr] = useState(null);
   const { article_id } = useParams();
 
   useEffect(() => {
@@ -18,6 +21,20 @@ const SingleArticle = () => {
     return <p>Loading...</p>;
   }
 
+  const incVotes = inc_votes => {
+    setVotes(currVotes => currVotes + inc_votes);
+    updateVote(article_id, inc_votes)
+      .then(updateArticle => {
+        setVotes(0);
+        setArticle(updateArticle);
+        setErr(null);
+      })
+      .catch(err => {
+        setVotes(0);
+        setErr(err.message);
+      });
+  };
+
   return (
     <>
       <article>
@@ -27,8 +44,10 @@ const SingleArticle = () => {
         <p>created on {article.created_at}</p>
       </article>
       <section>
-        <p>Votes: {article.votes}</p>
-        <button>good</button> <button>bad</button>
+        <p>Votes: {article.votes + votes}</p>
+        <button onClick={() => incVotes(1)}>Upvote</button>
+        <button onClick={() => incVotes(-1)}>Downvote</button>
+        {err ? <p>{err}</p> : null}
       </section>
     </>
   );
